160f117c297a93e287c273fc138d0d59d11acb86
omayma777emad <omimaemad777@gmail.com>
2023-11-16 09:19:24 -0800

    a

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,15 @@
+#include"main.h"
+
+/**
+ * print_alphabet - function that print alphabet in lower case
+ * Return: void
+*/
+
+void print_alphabet(void)
+{
+	int c;
+
+	for (c = 'a'; c <= 'z'; c++)
+		_putchar(c);
+	_putchar('\n');
+}
