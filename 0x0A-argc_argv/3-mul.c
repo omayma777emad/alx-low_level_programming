8a25ec471d080f4cac57f7244218bdfe55aa1fb1
omayma777emad <omimaemad777@gmail.com>
2023-11-19 10:40:53 -0800

    a

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -10,15 +10,15 @@
 
 int main(int argc, char *argv[])
 {
+	int i = atoi(argv[1]);
+	int j = atoi(argv[2]);
+	int r = i * j;
+
 	if (argc != 3)
 	{
 		printf("Error\n");
 	return (1);
 	}
-	int num1 = atoi(argv[1]);
-	int num2 = atoi(argv[2]);
-	int result = num1 * num2;
-
-	printf("%d\n", result);
+	printf("%d\n", r);
 	return (0);
 }
