44d1253a3ebcce099754e793abd6043f10c233c1
omayma777emad <omimaemad777@gmail.com>
2023-11-21 07:54:59 -0800

    hh

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -10,13 +10,12 @@
 char *create_array(unsigned int size, char c)
 {
 	unsigned int i;
+	char *array = malloc(size * sizeof(char));
 
 	if (size == 0)
 	{
 		return (NULL);
 	}
-	char *array = malloc(size * sizeof(char));
-
 	if (array == NULL)
 	{
 		return (NULL);
