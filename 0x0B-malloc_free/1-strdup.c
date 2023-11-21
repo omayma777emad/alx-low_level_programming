e645f488689a6a753de8d8595f2d69f4fcbe5165
omayma777emad <omimaemad777@gmail.com>
2023-11-21 08:50:38 -0800

    a

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,25 @@
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ *_strdup - function returns a pointer to a new string
+ * which is a duplicate of the string str
+ * @str: string
+ * Return: 0
+*/
+
+char *_strdup(char *str)
+{
+	char *duplicate = malloc(strlen(str) + 1);
+
+	if (str == NULL)
+	{
+		return (NULL);
+	}
+	if (duplicate == NULL)
+	{
+		return (NULL);
+	}
+	strcpy(duplicate, str);
+	return (duplicate);
+}
