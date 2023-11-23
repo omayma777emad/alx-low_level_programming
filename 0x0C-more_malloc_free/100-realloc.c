a5f2b8a1dea8ed00b277c435eb07cc38525e879f
omayma777emad <omimaemad777@gmail.com>
2023-11-23 09:40:00 -0800

    a

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -1,27 +1,39 @@
 #include <stdlib.h>
 #include <string.h>
 
-void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size) {
-    if (new_size == 0) {
-        free(ptr);
-        return NULL;
-    }
+/**
+ * *_realloc - function yhar reallocate the memory
+ * @old_size: int
+ * @new_size: int
+ * @ptr: pointer
+ * Return: pointer
+*/
 
-    if (ptr == NULL) {
-        return malloc(new_size);
-    }
+void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
+{
+	if (new_size == 0)
+	{
+		free(ptr);
+		return (NULL);
+	}
+	if (ptr == NULL)
+	{
+		return (malloc(new_size));
+	}
+	if (new_size == old_size)
+	{
+		return (ptr);
+	}
+	void *new_ptr = malloc(new_size);
 
-    if (new_size == old_size) {
-        return ptr;
-    }
+	if (new_ptr == NULL)
+	{
+		return (NULL);
+	}
+	unsigned int copy_size = old_size < new_size ? old_size : new_size;
 
-    void *new_ptr = malloc(new_size);
-    if (new_ptr == NULL) {
-        return NULL;
-    }
+	memcpy(new_ptr, ptr, copy_size);
 
-    unsigned int copy_size = old_size < new_size ? old_size : new_size;
-    memcpy(new_ptr, ptr, copy_size);
-    free(ptr);
-
-    return new_ptr
+	free(ptr);
+	return (new_ptr);
+}
