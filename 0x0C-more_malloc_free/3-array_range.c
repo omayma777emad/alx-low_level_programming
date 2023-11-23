000c65d70191ff0aeb738339d3d97b03a7daba12
omayma777emad <omimaemad777@gmail.com>
2023-11-23 09:05:58 -0800

    a

--- 0x0C-more_malloc_free/3-array_range.c
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -12,12 +12,13 @@
 int *array_range(int min, int max)
 {
 	int len, i;
-	int ptr;
+	int *ptr;
 
 	if (min > max)
 		return (NULL);
 	len = max - min + 1;
-	ptr = mallic(sizeof(int) * len);
+	ptr = malloc(sizeof(int) * len);
+
 	if (!ptr)
 		return (NULL);
 	for (i = 0; i < len; i++)
