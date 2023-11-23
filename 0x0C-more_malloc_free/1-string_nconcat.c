26fe21bb33bba6b8c04668c8d3b7f01b03aa3c9e
omayma777emad <omimaemad777@gmail.com>
2023-11-23 07:06:55 -0800

    a

---
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -0,0 +1,6 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+char *string_nconcat(char *s1, char *s2, unsigned int n)
+{
+
--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -1,9 +1,8 @@
 #ifndef MAIN_h
 #define  MAIN_h
 
-#include<stdlib.h>
-
 int _putchar(char c);
 void *malloc_checked(unsigned int b);
+char *string_nconcat(char *s1, char *s2, unsigned int n);
 
 #endif
