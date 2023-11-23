63695ae99e8822e3b1dece07393c7edcd4e209a5
omayma777emad <omimaemad777@gmail.com>
2023-11-23 08:08:13 -0800

    a

---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,45 @@
+#include"main.h"
+#include<stdio.h>
+#include<stdlib.h>
+
+/**
+ * *_memset - fill memory with conest byte
+ * @s: pointer
+ * @b: constant
+ * @n: max byte
+ * Return: s
+*/
+
+char *_memset(char *s, char b, unsigned int n)
+{
+	char *ptr = s;
+
+	while (n--)
+		*s++ = b;
+
+	return (ptr);
+}
+
+/**
+ * *_calloc - allocate memory for an array
+ * @nm: array length
+ * @size: size of each element
+ * Return: pointer
+*/
+
+void *_calloc(unsigned int nm, unsigned int size)
+{
+	void *m;
+
+	if (size == 0 || nm == 0)
+		return (NULL);
+	m = malloc(sizeof(int) * nm);
+
+	if (m == 0)
+		return (NULL);
+
+	_memset(m, 0, sizeof(int) * nm);
+
+	return (m);
+}
+
