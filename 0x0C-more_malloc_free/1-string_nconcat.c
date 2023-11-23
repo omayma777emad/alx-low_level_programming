9ae95c22ba9dee97591bae8ae698a1bf30466c72
omayma777emad <omimaemad777@gmail.com>
2023-11-23 08:36:35 -0800

    a

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -29,7 +29,7 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	{
 		return (NULL);
 	}
-	for (i = 0; s[i] != '\0'; i++)
+	for (i = 0; s1[i] != '\0'; i++)
 		t[i] = s1[i];
 	for (j = 0; j < n; j++)
 	{
