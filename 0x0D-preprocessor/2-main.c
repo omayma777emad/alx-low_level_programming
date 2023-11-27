ff1e67024975e1e7a1ae8acf9a86bf218857c82a
omayma777emad <omimaemad777@gmail.com>
2023-11-27 01:04:57 -0800

    a

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<stdlib>
+
+/**
+ * main - return the name of the file
+ *
+ * Return: 0
+*/
+
+int main(void)
+{
+	printf("the name of the file is %s", __file__);
+	return (0);
+}
