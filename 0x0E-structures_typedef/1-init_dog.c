33833268ef5c1f76596bc1f8cee8f4b1accd3953
omayma777emad <omimaemad777@gmail.com>
2023-11-27 07:46:20 -0800

    a

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include"dog.h"
+
+/**
+ * init_dog - function that initialize member of struct
+ * @d: struct member
+ * @name: dog name
+ * @age: dog age
+ * @owner: dog owner
+ * Return: void
+*/
+
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	d.name = name;
+	d.age = age;
+	d.owner = owner
+}
