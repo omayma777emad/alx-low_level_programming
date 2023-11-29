7dce1dd74a73879821e79a8d622b8aacfc41cae7
omayma777emad <omimaemad777@gmail.com>
2023-11-29 06:21:00 -0800

    a

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -10,7 +10,7 @@
 
 void array_iterator(int *array, size_t size, void (*action)(int))
 {
-	int i = array + size - 1;
+	int *i = array + size - 1;
 
 	if (array && size && action)
 		while (array <= i)
